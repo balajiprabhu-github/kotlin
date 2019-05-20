33453fe7866ba61acff38eb2b6623f9008fea98f
balajiprabhu <balajiprabhu008@gmail.com>
2019-05-20 12:22:16 +0530

    change water function for aquarium using compact function

--- src/aquarium/AMS.kt
+++ src/aquarium/AMS.kt
@@ -2,28 +2,26 @@ package aquarium
 
 import java.util.*
 
-fun main(){
+fun main() {
 
     println("Hello, Balaji")
     feedTheFish()
+
 }
 
 fun feedTheFish() {
+
     val day = randomDay()
     val food = fishFood(day)
     println("Today is $day and the fish eat $food")
-}
-
-fun randomDay(): String {
-
-    val week = listOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
-    return week[Random().nextInt(7)]
+    if (changeTheWater(day, 21, 30)) println("Change water !!")
 
 }
 
-fun fishFood(day : String) : String {
 
-    return when (day){
+fun fishFood(day: String): String {
+
+    return when (day) {
         "Sunday" -> "flakes"
         "Monday" -> "red worms"
         "Tuesday" -> "granules"
@@ -33,4 +31,31 @@ fun fishFood(day : String) : String {
         "Saturday" -> "green plants"
         else -> "Fasting"
     }
+}
+
+fun changeTheWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+
+    val isTooHot = temperature > 30
+    val isTooDirty = dirty > 30
+    val isSunday = day == "Sunday"
+
+    return when {
+        isTooHot -> true
+        isTooDirty -> true
+        isSunday -> true
+        else -> false
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isTooDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
+
+fun randomDay(): String {
+
+    val week = listOf("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+    return week[Random().nextInt(7)]
+
 }
