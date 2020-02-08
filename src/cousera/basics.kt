025ebf75773af7bb51b1ea42480a910b78e54bd0
balajiprabhu-github <balajiprabhu008@gmail.com>
2020-02-08 17:01:35 +0530

    kotlin var and val

--- src/cousera/basics.kt
+++ src/cousera/basics.kt
@@ -1,6 +1,11 @@
 package cousera
 
-fun main(args: Array<String>) {
-    val argument = print("Balaji")
-    println("Hello $argument")
+fun main() {
+
+    val languages = mutableListOf("Java")
+    languages.add("Kotlin") // No error
+
+    val cars = listOf("Mustang")
+    cars.add("Shelby") // Method not found error
+
 }
